6312ba1a40b8259d816b682b2f51c00b02b8d92f
ARROUI Sidahmed <sidahmedarroui@hotmail.com>
2019-12-26 15:46:35 +0100

    Wip Zoom

--- DataTrajec/Form1.cs
+++ DataTrajec/Form1.cs
@@ -30,7 +30,7 @@ namespace DataTrajec
             Init,
             LeftDrag,
             RightDrag,
-            WheelDrag
+            MiddleDrag
 
         }
         private State state;
@@ -44,7 +44,7 @@ namespace DataTrajec
         private Vector3 eye, target, up;
         private Vector2 previousPos;
         private int alphaValue;
-        private float length;
+        private float blend;
         public Trajectories()
         {
             InitializeComponent();
@@ -69,6 +69,7 @@ namespace DataTrajec
             myView.MouseMove += new MouseEventHandler(this.MouseMove);
             myView.MouseDown += new MouseEventHandler(this.MouseDown);
             myView.MouseUp += new MouseEventHandler(this.MouseUp);
+            myView.MouseWheel += new MouseEventHandler(this.MouseWheel);
             this.Controls.Add(myView);
         }
 
@@ -155,6 +156,57 @@ namespace DataTrajec
             return ((destMax - destMin) * (nb - srcMin) / (srcMax - srcMin)) + destMin;
         }
 
+        private new void MouseWheel(object sender, MouseEventArgs e)
+        {
+            /*Point p = convertScreenToWorldCoords(e.X, e.Y);
+            //blend = MathHelper.Clamp(blend, -1f, 1f);
+            blend = MathHelper.Clamp((0.001f * e.Delta), -1f, 1f);
+            Console.WriteLine("Blend : " + blend);
+            Matrix4 translation = Matrix4.CreateTranslation((target - eye) * blend);
+            eye = Vector3.TransformPosition(eye, translation);
+            myView.Invalidate();*/
+        }
+
+
+        // functions:
+        public static Point convertScreenToWorldCoords(int x, int y)
+        {
+            int[] viewport = new int[4];
+            Matrix4 modelViewMatrix, projectionMatrix;
+            GL.GetFloat(GetPName.ModelviewMatrix, out modelViewMatrix);
+            GL.GetFloat(GetPName.ProjectionMatrix, out projectionMatrix);
+            GL.GetInteger(GetPName.Viewport, viewport);
+            Vector2 mouse;
+            mouse.X = x;
+            mouse.Y = y;
+            Vector4 vector = UnProject(ref projectionMatrix, modelViewMatrix, new Size(viewport[2], viewport[3]), mouse);
+            Point coords = new Point((int)vector.X, (int)vector.Y);
+            return coords;
+        }
+        public static Vector4 UnProject(ref Matrix4 projection, Matrix4 view, Size viewport, Vector2 mouse)
+        {
+            Vector4 vec;
+
+            vec.X = 2.0f * mouse.X / (float)viewport.Width - 1;
+            vec.Y = 2.0f * mouse.Y / (float)viewport.Height - 1;
+            vec.Z = 0;
+            vec.W = 1.0f;
+
+            Matrix4 viewInv = Matrix4.Invert(view);
+            Matrix4 projInv = Matrix4.Invert(projection);
+
+            Vector4.Transform(ref vec, ref projInv, out vec);
+            Vector4.Transform(ref vec, ref viewInv, out vec);
+
+            if (vec.W > float.Epsilon || vec.W < float.Epsilon)
+            {
+                vec.X /= vec.W;
+                vec.Y /= vec.W;
+                vec.Z /= vec.W;
+            }
+
+            return vec;
+        }
         private new void MouseMove(object sender, MouseEventArgs e)
         {
 
@@ -162,11 +214,11 @@ namespace DataTrajec
             {
                 case State.LeftDrag:
                     state = State.LeftDrag;
-                    float deltaTheta = Map(0, myView.Width, 0, MathHelper.TwoPi, e.X - previousPos.X);
-                    float deltaPhi = Map(0, myView.Height, 0, MathHelper.TwoPi, e.Y - previousPos.Y);
+                    float deltaTheta = Map(0, myView.Width, 0, MathHelper.TwoPi, previousPos.X - e.X);
+                    float deltaPhi = Map(0, myView.Height, 0, MathHelper.Pi, previousPos.Y - e.Y);
 
-                    Quaternion rotation = new Quaternion(0, deltaTheta, deltaPhi);
-                    eye = Vector3.Transform(eye,rotation);
+                    Quaternion rotation = new Quaternion(deltaPhi, deltaTheta, 0);
+                    eye = Vector3.Transform(eye, rotation);
                     up = Vector3.Transform(up, rotation);
 
                     previousPos.X = e.X;
@@ -179,8 +231,8 @@ namespace DataTrajec
                     state = State.RightDrag;
 
                     break;
-                case State.WheelDrag:
-                    state = State.WheelDrag;
+                case State.MiddleDrag:
+                    state = State.MiddleDrag;
                     break;
                 default:
                     break;
@@ -230,7 +282,6 @@ namespace DataTrajec
                     previousPos.X = e.X;
                     previousPos.Y = e.Y;
                     break;
-
                 default:
                     break;
             }
@@ -240,7 +291,7 @@ namespace DataTrajec
             switch (state)
             {
                 case State.Init:
-                    state = State.WheelDrag;
+                    state = State.MiddleDrag;
                     previousPos.X = e.X;
                     previousPos.Y = e.Y;
                     break;
@@ -295,7 +346,7 @@ namespace DataTrajec
         {
             switch (state)
             {
-                case State.WheelDrag:
+                case State.MiddleDrag:
                     state = State.Init;
                     break;
                 default:
@@ -350,8 +401,8 @@ namespace DataTrajec
 
         private void InitValues()
         {
-            length = 3f;
-            eye = -length * Vector3.UnitX;
+            blend = 0f;
+            eye = -3f * Vector3.UnitX;
             target = Vector3.Zero;
             up = Vector3.UnitY;
             previousPos = Vector2.Zero;
